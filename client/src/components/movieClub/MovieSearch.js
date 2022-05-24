d9c43d128c4f504d3c6cf59e4a87ffecb6a73f9a
benskee <benskee33@gmail.com>
2022-05-23 20:17:05 -0400

    added get from movie api

--- client/src/components/movieClub/MovieSearch.js
+++ client/src/components/movieClub/MovieSearch.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import Input from '../common/Input';
+import React from 'react';
+import axios from 'axios';
+import Joi from 'joi-browser';
+import Form from './../common/Form';
 
-function MovieSearch() {
-  return (
-    <>
-    <Input />
-    </>
-  )
+export default class MovieSearch extends Form {
+        state = {
+        data: {search: ""},
+        errors: {},
+        movies: {}
+    }
+
+    schema = {
+        search: Joi.string()
+    }
+
+    doSubmit = () => {
+        const movie = this.state.data.search
+        const api_key = process.env.REACT_APP_MOVIE_API_KEY;
+        let movieData = [];
+
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${movie}&include_adult=true`, {transformRequest: (data, headers) => {
+            delete headers.common['x-auth-token'];
+            return data;
+        }})
+        .then(res => {
+            console.log(res.data.results)
+            movieData.push(res.data.results[0])
+            this.setState({
+                movies: movieData
+            })
+        })
+        
+        console.log(this.state.movies)
+    }
+
+    render() {
+        return (
+            <>
+                <br/> Movie search
+                <form onSubmit={this.handleSubmit} className="col-6 m-auto">
+                    {this.renderInput('search', 'Search for a movie...')}
+                    {this.renderButton('Search')}
+                </form>
+            </>
+        )
+    }
 }
-
-export default MovieSearch
