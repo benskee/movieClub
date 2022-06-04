dbf58b1771ae66d78f2106ac8e76d6b25b4c8f43
benskee <benskee33@gmail.com>
2022-06-03 21:38:17 -0400

    Home.js functional component

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import ImageLink from '../components/common/ImageLink';
 
-export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <div className="container">
-                    <h1>Welcome to the 3kp App</h1>
-                    <h5 className='homeTitleSubtext'>Where Reasonable people hang out</h5>
-                    <h1><br /></h1>
-                </div>
-                
-                <Link to='/movieClub'>
-                    <h3 className='col-2 m-auto border border-primary p-4'>Movie Club</h3>
-                </Link>
+function Home() {
+    return (
+        <div>
+            <div className="container">
+                <h1>Welcome to the 3kp App</h1>
+                <h5 className='homeTitleSubtext'>Where Reasonable people hang out</h5>
+                <h1><br /></h1>
             </div>
-        );
-    }
+            
+            <Link to='/movieClub'>
+                <h3 className='col-2 m-auto border border-primary p-4'>Movie Club</h3>
+            </Link>
+        </div>
+    );
 }
+
+export default Home
