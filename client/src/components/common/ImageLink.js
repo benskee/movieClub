090353c45a841fab703e9e4f42cc7632737d80be
benskee <benskee33@gmail.com>
2022-06-22 21:29:31 -0400

    ImageLink functional component

--- client/src/components/common/ImageLink.js
+++ client/src/components/common/ImageLink.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React  from 'react'
 import { Link } from 'react-router-dom';
 
-export default class ImageLink extends Component {
-    render() {
-        const { link, label, image} = this.props
-        return (
-            <React.Fragment>
-                <div className="col-md-4 col-sm-6">
-                    <div className="container mt-4 mr-auto ml-auto">
-                        <Link to={link} style={{ textDecoration: 'none', color: 'black' }}>
-                            <h3 style={{ textAlign: 'center'}}>{label}</h3>
-                            <img alt="" className="imageLink img-fluid pic-1" style={{ height: '150px', display: 'block', marginLeft: 'auto', marginRight: 'auto' }} src={`./static/images/${image}_image.png`}></img>
-                        </Link>
-                    </div>
+function ImageLink(props) {
+    const { link, label, image } = props
+    return (
+        <>
+            <div className="col-md-4 col-sm-6">
+                <div className="container mt-4 d-flex justify-content-center">
+                    <Link to={link} className='text-dark'>
+                        <h3 className="text-center">{label}</h3>
+                        <img alt={`link for ${image}`} className="imageLink img-fluid pic-1" src={`./static/images/${image}_image.png`}></img>
+                    </Link>
                 </div>
-            </React.Fragment>
-        )
-    }
+            </div>
+        </>
+    )
 }
+
+export default ImageLink
